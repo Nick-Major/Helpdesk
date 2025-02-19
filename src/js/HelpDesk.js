14e8db2de286bee64f34b10b34c92bbce3360bd0
Nick-Major <divanchik26@gmail.com>
2025-02-19 16:01:23 +0300

    Control panel markup added

--- src/js/HelpDesk.js
+++ src/js/HelpDesk.js
@@ -2,15 +2,57 @@
  *  Основной класс приложения
  * */
 export default class HelpDesk {
-  constructor(container, ticketService) {
+  constructor(container, ticketService, ticketView, ticketForm, deleteTicketForm) {
     if (!(container instanceof HTMLElement)) {
       throw new Error('This is not HTML element!');
     }
     this.container = container;
     this.ticketService = ticketService;
+    this.tickets = [];
+    this.ticketView = ticketView;
+    this.ticketForm = ticketForm;
+    this.deleteTicketForm = deleteTicketForm;
   }
 
   init() {
-    console.info('init');
+    this.addControlPanel();
+    this.loadTickets();
+    this.render();
+    this.addEventListeners();
   }
+
+  addControlPanel() {
+    const controlPanel = document.createElement('div');
+    controlPanel.classList.add('control-panel');
+
+    const dotsContainer = document.createElement('div');
+    dotsContainer.classList.add('dots-container');
+
+    for(let i = 0; i < 3; i++) {
+      const newEl = document.createElement('span');
+      newEl.classList.add('dot');
+      dotsContainer.appendChild(newEl);
+    }
+
+    controlPanel.appendChild(dotsContainer);
+
+    const addBtnContainer = document.createElement('div');
+    addBtnContainer.classList.add('add-btn-container');
+
+    const addTicketBtn = document.createElement('button');
+    addTicketBtn.classList.add('add-ticket-btn', 'btn');
+    addTicketBtn.textContent = 'Добавить тикет';
+
+    addBtnContainer.appendChild(addTicketBtn);
+
+    controlPanel.appendChild(addBtnContainer);
+
+    const ticketList = document.createElement('div');
+    ticketList.classList.add('ticket-list');
+
+    controlPanel.appendChild(ticketList);
+
+    this.container.append(controlPanel);
+  }
+
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,71 @@ import TicketService from './TicketService';
 import TicketView from './TicketView';
 
 const root = document.getElementById('root');
+
+const controlPanel = document.createElement('div');
+controlPanel.classList.add('control-panel');
+
+const dotsContainer = document.createElement('div');
+dotsContainer.classList.add('dots-container');
+
+for(let i = 0; i < 3; i++) {
+    const newEl = document.createElement('span');
+    newEl.classList.add('dot');
+    dotsContainer.appendChild(newEl);
+}
+
+controlPanel.appendChild(dotsContainer);
+
+const addBtnContainer = document.createElement('div');
+addBtnContainer.classList.add('add-btn-container');
+
+const addTicketBtn = document.createElement('button');
+addTicketBtn.classList.add('add-ticket-btn', 'btn');
+addTicketBtn.textContent = 'Добавить тикет';
+
+addBtnContainer.appendChild(addTicketBtn);
+
+controlPanel.appendChild(addBtnContainer);
+
+const ticketList = document.createElement('div');
+ticketList.classList.add('ticket-list');
+
+controlPanel.appendChild(ticketList);
+
+root.append(controlPanel);
+
+
+// const body = document.querySelector('body');
+
+// const controlPanel = document.createElement('div');
+// controlPanel.classList.add('control-panel');
+
+// const dotsContainer = document.createElement('div');
+// dotsContainer.classList.add('dots-container');
+
+// for(let i = 0; i < 3; i++) {
+//     const newEl = document.createElement('span');
+//     newEl.classList.add('dot');
+//     dotsContainer.appendChild(newEl);
+// }
+
+// controlPanel.appendChild(dotsContainer);
+
+// const addBtnContainer = document.createElement('div');
+// addBtnContainer.classList.add('add-btn-container');
+
+// const addTicketBtn = document.createElement('button');
+// addTicketBtn.classList.add('add-ticket-btn', 'btn');
+// addTicketBtn.textContent = 'Добавить тикет';
+
+// addBtnContainer.appendChild(addTicketBtn);
+
+// controlPanel.appendChild(addBtnContainer);
+
+// body.insertBefore(controlPanel, root);
+
+
+
 // const ticketService = new TicketService();
 
 // const app = new HelpDesk(root, ticketService);
